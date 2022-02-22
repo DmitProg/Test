90c14694e343819158dabb87f093370b9c028f6e
DmitProg <dmonk30@gmail.com>
2022-02-22 13:23:47 +0200

    My first commit

---
+++ IF.py
@@ -0,0 +1,98 @@
+"""a = 12
+b = 332
+c = 888
+
+m = a
+if b>m:
+    m = b
+if c>m:
+    m = c
+print(m)"""
+
+
+"""a = 12
+b = 332
+c = 888
+
+if a<b and c>b:
+    print(a)"""
+
+
+"""a=12
+b=332
+c=888
+e=1
+g=8
+
+if b>a:
+    if c<a:
+        print(a)
+    else:
+        print(c)"""
+
+
+"""print ("inset password")
+pas = input()
+if pas == "1234":
+    print ("u r in")
+else:
+    print ("wrong password")"""
+
+
+"""answer = 2+2*2
+if answer == 12:
+    print("answers are equals")
+else:
+    print("answers are not equals")"""
+
+"""answer = 11
+if answer <= 12:
+    print("bingo")
+else:
+    print("u loose")"""
+    
+
+"""import random
+
+value = random.randint(0,5)
+if value == 5:
+    print(5)
+elif value == 4:
+    print(4)
+elif value == 3:
+    print(3)
+elif value == 2:
+    print(2)
+elif value == 1:
+    print(1)
+else:
+    print(0)"""
+
+#different way of above task
+
+"""import random
+value = random.randint(0,5)
+if value == 5:
+    print(5)
+if value == 4:
+    print(4)
+if value == 3:
+    print(3)
+if value == 2:
+    print(2)
+if value == 1:
+    print(1)
+if value == 0:
+    print(0)"""
+
+
+"""pas1 = "1234"
+pas2 = "123456"
+print ("Please your password 1")
+userInput1 = input()
+print ("Please your password 2")
+userInput2 = input()
+if userInput1 == pas1 and userInput2 == pas2:
+    print("Congratulations")
+else:
+    print("Access denied")"""
